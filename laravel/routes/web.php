3e84a0c46f08eda5a06529d4efd1c4c901095e96
Muhammad Nur Amien Rais <56391596+amnnrth@users.noreply.github.com>
2022-05-13 12:45:57 +0700

    install jet, foldering, and create file.
    install jetstream.
    creating folder and created file blade

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -31,3 +31,13 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('users', UserController::class);
     Route::resource('products', BelajarController::class);
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
